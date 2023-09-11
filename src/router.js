6b4cf9df161eec181e7579edaea0c87abfa50963
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-09-11 10:07:43 +0700

    Wiring up pages

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-// import CoachDetail from "./pages/coaches/CoachDetail.vue";
+import CoachDetail from "./pages/coaches/CoachDetail.vue";
 import CoachesList from "./pages/coaches/CoachesList.vue";
-// import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
 
-// import ContactCoach from "./pages/requests/ContactCoach.vue";
-// import RequestsReceived from "./pages/requests/RequestsReceived.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue";
 
-// import NotFound from "./pages/NotFound.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,28 +20,28 @@ const router = createRouter({
       path: "/coaches",
       component: CoachesList,
     },
-    // {
-    //   path: "/coaches/:id",
-    //   component: CoachDetail,
-    //   children: [
-    //     {
-    //       path: "contact",
-    //       component: ContactCoach,
-    //     },
-    //   ],
-    // },
-    // {
-    //   path: "/register",
-    //   component: CoachRegistration,
-    // },
-    // {
-    //   path: "/requests",
-    //   component: RequestsReceived,
-    // },
-    // {
-    //   path: "/:notFound(.*)",
-    //   component: NotFound,
-    // },
+    {
+      path: "/coaches/:id",
+      component: CoachDetail,
+      children: [
+        {
+          path: "contact",
+          component: ContactCoach,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      component: CoachRegistration,
+    },
+    {
+      path: "/requests",
+      component: RequestsReceived,
+    },
+    {
+      path: "/:notFound(.*)",
+      component: NotFound,
+    },
   ],
 });
 
