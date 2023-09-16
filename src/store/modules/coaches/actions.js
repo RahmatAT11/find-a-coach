c519dcc1b74d44805a574348bccba00385a0f0bc
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-09-16 09:15:04 +0700

    Fetching coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -21,12 +21,38 @@ export default {
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData)
+      console.log(responseData);
     }
 
     context.commit("registerCoach", {
       ...coachData,
       id: userId,
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-http-demo-60fad-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+
+      coaches.push(coach);
+    }
+
+    context.commit("setCoaches", coaches);
   },
 };
