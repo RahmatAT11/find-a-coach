c4c813c77ebe97c952e20d386131e3e4728969e1
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-09-17 10:41:44 +0700

    Attach token to outgoing request

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -10,8 +10,10 @@ export default {
       areas: data.areas,
     };
 
+    const token = context.rootGetters.token
+
     const response = await fetch(
-      `https://vue-http-demo-60fad-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`,
+      `https://vue-http-demo-60fad-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json?auth=${token}`,
       {
         method: "PUT",
         body: JSON.stringify(coachData),
