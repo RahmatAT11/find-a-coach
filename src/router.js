815c3a691608e3271b62543a4f72723cb0cfc794
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-09-17 16:06:59 +0700

    Revert back from asyn component

--- src/router.js
+++ src/router.js
@@ -1,5 +1,3 @@
-import { defineAsyncComponent } from "vue";
-
 import { createRouter, createWebHistory } from "vue-router";
 
 // import CoachDetail from "./pages/coaches/CoachDetail.vue";
@@ -15,21 +13,11 @@ import NotFound from "./pages/NotFound.vue";
 
 import store from "./store/index";
 
-const CoachDetail = defineAsyncComponent(() =>
-  import("./pages/coaches/CoachDetail.vue")
-);
-const CoachRegistration = defineAsyncComponent(() =>
-  import("./pages/coaches/CoachRegistration.vue")
-);
-const ContactCoach = defineAsyncComponent(() =>
-  import("./pages/requests/ContactCoach.vue")
-);
-const RequestsReceived = defineAsyncComponent(() =>
-  import("./pages/requests/RequestsReceived.vue")
-);
-const UserAuth = defineAsyncComponent(() =>
-  import("./pages/auth/UserAuth.vue")
-);
+const CoachDetail = () => import("./pages/coaches/CoachDetail.vue");
+const CoachRegistration = () => import("./pages/coaches/CoachRegistration.vue");
+const ContactCoach = () => import("./pages/requests/ContactCoach.vue");
+const RequestsReceived = () => import("./pages/requests/RequestsReceived.vue");
+const UserAuth = () => import("./pages/auth/UserAuth.vue");
 
 const router = createRouter({
   history: createWebHistory(),
